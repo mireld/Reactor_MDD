0588bbb1dbf6c7e311bd994304150dc1c09140cd
mireld <davidmirelesluis@gmail.com>
2023-03-03 12:29:44 -0600

    add the function-resta

--- Calculadora.py
+++ Calculadora.py
@@ -5,5 +5,9 @@ def suma(numero1, numero2):
     total = numero1 + numero2
     return total
 
-resultado = suma(num1,num2)
-print('El resultado es: ', resultado)
+def resta(numero1, numero2):
+    total = numero1 - numero2
+    return total
+
+resultado = [suma(num1,num2),resta(num1,num2)]
+print('El resultado de la suma es: ', resultado[0], ', y la resta es: ',resultado[1])
